6ecfc9133684ae9214f6b5d0514181c2a23a199c
Sunny <Sunnylaw3571@gmail.com>
2024-04-30 15:44:51 +0100

    task 9 method created

--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -4,11 +4,15 @@ import java.util.Map;
 public class ShoppingCart {
     private Map<String, Double> items = new HashMap<>();
 
-    public void addItem(String item, Double price){
-        items.put(item,price);
+    public void addItem(String item, Double price) {
+        items.put(item, price);
     }
 
-    public void calculatePrice(Map<String, Double> input){
-        for ()
+    public double calculatePrice(Map<String, Double> input) {
+        double totalPrice = 0;
+        for (Map.Entry<String, Double> entry : input.entrySet()) {
+            totalPrice += entry.getValue();
+        }
+        return totalPrice;
     }
 }
