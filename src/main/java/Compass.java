5e847cfc5c1270e86d27865076432ad0ea67ed58
Sam Kettle <sam.kettle3@icloud.com>
2024-04-30 11:13:19 +0100

    feat: complete Compass.rotate method and passing test

--- src/main/java/Compass.java
+++ src/main/java/Compass.java
@@ -15,6 +15,36 @@ public class Compass {
     private Point point;
 
     public static Point rotate(Point point, Direction direction){
-        return Point.EAST;
+        switch(point) {
+            case NORTH -> {
+                if (direction.equals(Direction.LEFT)) {
+                    return Point.WEST;
+                } else {
+                    return Point.EAST;
+                }
+            }
+            case EAST -> {
+                if (direction.equals(Direction.LEFT)) {
+                    return Point.NORTH;
+                } else {
+                    return Point.SOUTH;
+                }
+            }
+            case SOUTH -> {
+                if (direction.equals(Direction.LEFT)) {
+                    return Point.EAST;
+                } else {
+                    return Point.WEST;
+                }
+            }
+            case WEST -> {
+                if (direction.equals(Direction.LEFT)) {
+                    return Point.SOUTH;
+                } else {
+                    return Point.NORTH;
+                }
+            }
+        }
+        return null;
     }
 }
