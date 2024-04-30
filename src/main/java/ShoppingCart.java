b8d81af3730b093e4a8db599c2f3088da5cfc0fa
Sunny <Sunnylaw3571@gmail.com>
2024-04-30 15:38:11 +0100

    working on task 9

---
+++ src/main/java/ShoppingCart.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ShoppingCart {
+    private Map<String, Double> items = new HashMap<>();
+
+    public void addItem(String item, Double price){
+        items.put(item,price);
+    }
+
+    public void calculatePrice(Map<String, Double> input){
+        for ()
+    }
+}
