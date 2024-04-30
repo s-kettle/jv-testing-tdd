6e7086671c8506cd44eae2503e4946eb320f1966
Sam Kettle <sam.kettle3@icloud.com>
2024-04-30 15:06:10 +0100

    feat: attempt at findLongestWords() implementation

--- src/main/java/WordAnalyser.java
+++ src/main/java/WordAnalyser.java
@@ -1,9 +1,29 @@
+import java.util.ArrayList;
 import java.util.Map;
 
 public class WordAnalyser {
     public String[] findLongestWords(String text) {
         // TODO: Implement the logic to find the longest word(s) in the given text
-        return new String[]{""};
+
+        String[] inputToArray = text
+                .replaceAll("/[^a-zA-Z0-9 ]*/g", "")
+                .split(" ");
+
+        ArrayList<String> stringList = new ArrayList<>();
+        int longestLength = inputToArray[0].length();
+
+        for (int n = 1; n < inputToArray.length; n++ ) {
+            if (inputToArray[n].length() > longestLength) {
+                longestLength = inputToArray[n].length();
+            }
+        }
+        for (String word : inputToArray) {
+            if (word.length() == longestLength) {
+                stringList.add(word);
+            }
+        }
+
+        return stringList.toArray(new String[0]);
     }
 
     public Map<Character, Integer> calculateLetterFrequency(String text) {
